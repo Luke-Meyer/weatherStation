f95a745afe9d5564531eed650d1dc9bc59797890
unknown <1640636@T-SMD1020394.SDSMT.local>
2016-02-29 08:42:39 -0700

    added some members to Day.java for stat tracking at the day level

--- parsing/Day.java
+++ parsing/Day.java
@@ -6,6 +6,15 @@ public class Day
 	private int month;
 	private int year;
 	private ArrayList<wItem> samples;
+	private float meanTemp;
+	private float highTemp;
+	private String hTempDate;
+	private float lowTemp;
+	private String lTempDate;
+	private float meanWind;
+	private float maxWind;
+	private String mWindDate;
+	private float rainfall;
 	
 	public Day()
 	{
@@ -15,6 +24,78 @@ public class Day
 		this.samples = new ArrayList<wItem>();
 	}
 	
+	public float getMeanTemp()
+	{
+		return this.meanTemp;
+	}
+	
+	public void setMeanTemp( float avg )
+	{
+		this.meanTemp = avg;
+	}
+	
+	public float getHighTemp()
+	{
+		return this.highTemp;
+	}
+	
+	public void setHighTemp( float high, String date )
+	{
+		this.highTemp = high;
+		this.hTempDate = date;
+	}
+	
+	public String getHighTempDate()
+	{
+		return this.hTempDate;
+	}
+	
+	public float getLowTemp()
+	{
+		return this.lowTemp;
+	}
+	
+	public void setLowTemp( float high, String date )
+	{
+		this.lowTemp = high;
+		this.lTempDate = date;
+	}
+	
+	public String getLowTempDate()
+	{
+		return this.lTempDate;
+	}
+	
+	public float getMeanWind()
+	{
+		return this.meanWind;
+	}
+	
+	public void setMeanWind( float avg )
+	{
+		this.meanWind = avg;
+	}
+	
+	public float getMaxWind()
+	{
+		return this.maxWind;
+	}
+	
+	public void setMaxWind( float max, String date )
+	{
+		this.maxWind = max;
+		this.mWindDate = date;
+	}
+	
+	public float getRainfall()
+	{
+		return this.rainfall;
+	}
+	
+	public void setRainfall( float rain )
+	{
+		this.rainfall = rain;
+	}
 	
 	public int getDay()
 	{
