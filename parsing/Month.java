5690b7556b272e2ff75d1589225d40bc740fdd2f
unknown <1640636@T-SMD1020394.SDSMT.local>
2016-02-29 21:35:52 -0700

    added weekStats class to Month.java for weekly stat calculation

--- parsing/Month.java
+++ parsing/Month.java
@@ -10,9 +10,11 @@ public class WeeklyStats
 	private float minTemp;
 	private String minTempDate;
 	private String minTempTime;
-	//private 
-	
-	//private 
+	private float meanWind;
+	private float maxWind;
+	private String windDate;
+	private String windTime;
+    private float rainfall;
 	
 }
 */
@@ -23,6 +25,8 @@ public class Month
 	private int year;
 	private Hashtable<Integer, Day> dailySamples;
 	private Hashtable<Integer, ArrayList<Day> > weeklySamples;
+	
+	private Hashtable<Integer, ArrayList<WeekStats>> weekStats;
 	
 	
 	private float meanTemp;
@@ -305,12 +309,6 @@ public class Month
 		return this.weeklySamples.get( weekKey );
 	}
 	
-	/*
-	public float getWeekMeanTemp()
-	{
-		
-	}
-	*/
 	
 	public int getMonth()
 	{
@@ -354,8 +352,120 @@ public class Month
 		this.year = -1;
 	}
 	
-	/*(public ArrayList<Day> getWeekOfSamples( int weekKey )
+	public class WeekStats
 	{
+		private float meanTemp;
+		private float maxTemp;
+		private String maxTempDate;
+		private String maxTempTime;
+		private float minTemp;
+		private String minTempDate;
+		private String minTempTime;
+		private float meanWind;
+		private float maxWind;
+		private String windDate;
+		private String windTime;
+		private float rainfall;
 		
-	}*/
+		
+		public float getMeanTemp()
+		{
+			return this.meanTemp;
+		}
+		
+		public void setMeanTemp( float avg)
+		{
+			this.meanTemp = avg;
+		}
+		
+		public float getMaxTemp()
+		{
+			return this.maxTemp;
+		}
+		
+		public void setMaxTemp( float max, String date, String time )
+		{
+			this.maxTemp = max;
+			this.maxTempDate = date;
+			this.maxTempTime = time;
+		}
+		
+		public String getMaxTempDate()
+		{
+			return this.maxTempDate;
+		}
+		
+		
+		public String getMaxTempTime()
+		{
+			return this.maxTempTime;
+		}
+		
+		public float getLowTemp()
+		{
+			return this.minTemp;
+		}
+		
+		public void setLowTemp( float low, String date, String time )
+		{
+			this.minTemp = low;
+			this.minTempDate = date;
+			this.minTempTime = time;
+		}
+		
+		public String getLowTempDate()
+		{
+			return this.minTempDate;
+		}
+		
+		public String getLowTempTime()
+		{
+			return this.minTempTime;
+		}
+		
+		public float getMeanWind()
+		{
+			return this.meanWind;
+		}
+		
+		public void setMeanWind( float avg )
+		{
+			this.meanWind = avg;
+		}
+		
+		public float getMaxWind()
+		{
+			return this.maxWind;
+		}
+		
+		public void setMaxWind( float max, String date, String time )
+		{
+			this.maxWind = max;
+			this.windDate = date;
+			this.windTime = time;
+		}
+		
+		public String getMaxWindDate()
+		{
+			return this.windDate;
+		}
+		
+		public String getMaxWindTime()
+		{
+			return this.windTime;
+		}
+		
+		public float getRainfall()
+		{
+			return this.rainfall;
+		}
+		
+		public void setRainfall( float rain )
+		{
+			this.rainfall = rain;
+		}
+		
+	
+	}
+	
 }
