e0cc26f2303c56d95c72af9961329c863c27b599
turkk33 <turkk@localhost.localdomain>
2016-03-02 00:12:01 -0700

    comments

--- Weather Station/src/weather/station/Month.java
+++ Weather Station/src/weather/station/Month.java
@@ -2,9 +2,16 @@ package weather.station;
 
 import java.util.*;
 
-
+/************************************************************************
+    Class:  Month
+    Author: All
+    Description: This class creates month data from the parsed xml data
+    * that is read in to the program.
+    Parameters: None
+ ************************************************************************/
 public class Month
 {
+        //private members and containers
 	private int month;
 	private int year;
 	private Hashtable<Integer, Day> dailySamples;
@@ -30,7 +37,12 @@ public class Month
 	private String mWindTime;
 	private float rainfall;
 	
-	
+	/************************************************************************
+            Function:  Month
+            Author: All
+            Description: This is the constructor that instantiates the containers
+            Parameters: None
+         ************************************************************************/
 	public Month()
 	{
 		this.dailySamples = new Hashtable<Integer, Day>();
@@ -40,6 +52,13 @@ public class Month
 		this.year = -1;
 	}
 	
+        /************************************************************************
+            Function:  Month
+            Author: All
+            Description: This is an overloaded constructor that instantiates 
+            * the containers and certain data.
+            Parameters: tempMonth - the month currently being handled
+         ************************************************************************/
 	public Month( Month tempMonth )
 	{
 		this.dailySamples = new Hashtable<Integer, Day>();
@@ -54,11 +73,23 @@ public class Month
 
 	}
 	
+        /************************************************************************
+            Function:  getPrevailingWindDir
+            Author: All
+            Description: Getter to get the prevailing wind direction.
+            Parameters: none
+         ************************************************************************/
 	public String getPrevailingWindDir()
 	{
 		return this.prevailingWindDir;
 	}
 	
+        /************************************************************************
+            Function:  getWeekPrevailingWindDir
+            Author: All
+            Description: Getter to get the prevailing wind direction for a week.
+            Parameters: weekIndex - the index into the week data
+         ************************************************************************/
 	public String getWeekPrevailingWindDir( int weekIndex )
 	{
 		String dir = this.weekPrevailingWindDir.get( weekIndex );
@@ -67,7 +98,12 @@ public class Month
 		
 	}
 	
-	
+	/************************************************************************
+            Function:  getSampleCount
+            Author: All
+            Description: Gets the number of samples in the dataset.
+            Parameters: none
+         ************************************************************************/
 	public int getSampleCount()
 	{
 		int count = 0;
@@ -88,13 +124,28 @@ public class Month
 		return count;
 	}
 	
-	
+	/************************************************************************
+            Function:  getWeekStats
+            Author: All
+            Description: Getter to get the prevailing wind direction.
+            Parameters: weekIndex - index to access week data
+         ************************************************************************/
 	public WeekStats getWeekStats( int weekIndex )
 	{
 		return this.weekStats.get(weekIndex);
 	}
 	
 	
+        /************************************************************************
+            Function:  calcWeekStats
+            Author: All
+            Description: calculates all the needed statistics for a week's 
+            * period of time.  Calculates min, max, and average temperature for 
+            * the week. Calculates the average and high wind from the week.
+            * Also calculates average wind wind direction. Finally calculates
+            * total rainfall for the week.
+            Parameters: none
+         ************************************************************************/
 	public void calcWeekStats()
 	{
 		float tempSum = 0.0f;
@@ -486,22 +537,47 @@ public class Month
 		
 	}
 	
-	
+	/************************************************************************
+            Function:  getMeanTemp
+            Author: All
+            Description: Getter for mean temperature of a dataset
+            Parameters: none
+         ************************************************************************/
 	public float getMeanTemp()
 	{
 		return this.meanTemp;
 	}
 	
+        /************************************************************************
+            Function:  setMeanTemp
+            Author: All
+            Description: Set the mean temperature for a dataset
+            Parameters: avg - holds the mean data value
+         ************************************************************************/
 	public void setMeanTemp( float avg )
 	{
 		this.meanTemp = avg;
 	}
 	
+        /************************************************************************
+            Function:  getHighTemp
+            Author: All
+            Description: Getter for the high temperature of a dataset
+            Parameters: none
+         ************************************************************************/
 	public float getHighTemp()
 	{
 		return this.highTemp;
 	}
 	
+        /************************************************************************
+            Function:  setHighTemp
+            Author: All
+            Description: Sets high temperature for a dataset
+            Parameters: high - high temperature
+            * date - date the high happened
+            * time - time the high happened
+         ************************************************************************/
 	public void setHighTemp( float high, String date, String time )
 	{
 		this.highTemp = high;
@@ -509,21 +585,47 @@ public class Month
 		this.hTempTime = time;
 	}
 	
+        /************************************************************************
+            Function:  getHighTempDate
+            Author: All
+            Description: Gets date of the high temperature
+            Parameters: none
+         ************************************************************************/
 	public String getHighTempDate()
 	{
 		return this.hTempDate;
 	}
 	
+        /************************************************************************
+            Function:  getHighTempTime
+            Author: All
+            Description: Gets time of the high temperature
+            Parameters: none
+         ************************************************************************/
 	public String getHighTempTime()
 	{
 		return this.hTempTime;
 	}
 	
+        /************************************************************************
+            Function:  getLowTemp
+            Author: All
+            Description: Gets low temperature of a dataset
+            Parameters: none
+         ************************************************************************/
 	public float getLowTemp()
 	{
 		return this.lowTemp;
 	}
 	
+        /************************************************************************
+            Function:  setLowTemp
+            Author: All
+            Description: Sets low temperature of a dataset
+            Parameters: high - low temperature
+            * date - date the low happened
+            * time - time the low happened
+         ************************************************************************/
 	public void setLowTemp( float high, String date, String time )
 	{
 		this.lowTemp = high;
@@ -531,31 +633,69 @@ public class Month
 		this.lTempTime = time;
 	}
 	
+        /************************************************************************
+            Function:  getLowTempDate
+            Author: All
+            Description: Get the date of the low temp of a dataset
+            Parameters: none
+         ************************************************************************/
 	public String getLowTempDate()
 	{
 		return this.lTempDate;
 	}
 	
+        /************************************************************************
+            Function:  getLowTempTime
+            Author: All
+            Description: Gets the time of the low temp of a dataset
+            Parameters: none
+         ************************************************************************/
 	public String getLowTempTime()
 	{
 		return this.lTempTime;
 	}
 	
+        /************************************************************************
+            Function:  getMeanWind
+            Author: All
+            Description: Gets average wind speed of a dataset
+            Parameters: none
+         ************************************************************************/
 	public float getMeanWind()
 	{
 		return this.meanWind;
 	}
 	
+        /************************************************************************
+            Function:  setMeanWind
+            Author: All
+            Description: set average wind speed for a dataset
+            Parameters: avg - average wind speed
+         ************************************************************************/
 	public void setMeanWind( float avg )
 	{
 		this.meanWind = avg;
 	}
 	
+        /************************************************************************
+            Function:  getMaxWind
+            Author: All
+            Description: Gets maximum wind speed for a set of data
+            Parameters: none
+         ************************************************************************/
 	public float getMaxWind()
 	{
 		return this.maxWind;
 	}
 	
+        /************************************************************************
+            Function:  setMaxWind
+            Author: All
+            Description: Sets maximum wind direction for a set of data
+            Parameters:  max - maximum wind speed
+            * date - date the maximum happened
+            * time - time the maximum happened
+         ************************************************************************/
 	public void setMaxWind( float max, String date, String time )
 	{
 		this.maxWind = max;
@@ -563,35 +703,69 @@ public class Month
 		this.mWindTime = time;
 	}
 	
+        /************************************************************************
+            Function:  getMaxWindDate
+            Author: All
+            Description: Gets date the max wind happened
+            Parameters: none
+         ************************************************************************/
 	public String getMaxWindDate()
 	{
 		return this.mWindDate;
 	}
 	
+        /************************************************************************
+            Function:  getMaxWindTime
+            Author: All
+            Description: Gets time the max wind happened
+            Parameters: none
+         ************************************************************************/
 	public String getMaxWindTime()
 	{
 		return this.mWindTime;
 	}
 	
+        /************************************************************************
+            Function:  getRainfall
+            Author: All
+            Description: Gets total rainfall for a dataset
+            Parameters: none
+         ************************************************************************/
 	public float getRainfall()
 	{
 		return this.rainfall;
 	}
 	
+        /************************************************************************
+            Function:  setRainfall
+            Author: All
+            Description: Sets rainfall total for a datset
+            Parameters: none
+         ************************************************************************/
 	public void setRainfall( float rain )
 	{
 		this.rainfall = rain;
 	}
 	
 	
-	
+	/************************************************************************
+            Function:  getNumberOfDays
+            Author: All
+            Description: Gets number of days in sample set
+            Parameters: none
+         ************************************************************************/
 	public int getNumberOfDays()
 	{
 		return this.dailySamples.size();
 	}
 	
 	
-	
+	/************************************************************************
+            Function:  setWeeklySamples
+            Author: All
+            Description: Sets the week long data sets
+            Parameters: none
+         ************************************************************************/
 	public void setWeeklySamples()
 	{
 		
@@ -631,53 +805,112 @@ public class Month
 
 	}
 	
-	
+	/************************************************************************
+            Function:  getAllWeekSamples
+            Author: All
+            Description: gets all week long data samples
+            Parameters: none
+         ************************************************************************/
 	public Hashtable<Integer, ArrayList<Day> > getAllWeekSamples()
 	{
 		return this.weeklySamples;
 	}
 	
+        /************************************************************************
+            Function:  getWeekOfSamples
+            Author: All
+            Description: Gets the week of samples from the given index
+            Parameters: weekKey - week index of the data
+         ************************************************************************/
 	public ArrayList<Day> getWeekOfSamples( int weekKey )
 	{
 		return this.weeklySamples.get( weekKey );
 	}
 	
-	
+	/************************************************************************
+            Function:  getMonth
+            Author: All
+            Description: Gets the month the data set is in
+            Parameters: none
+         ************************************************************************/
 	public int getMonth()
 	{
 		return this.month;
 	}
 	
+        /************************************************************************
+            Function:  setMonth
+            Author: All
+            Description: Set data to given month
+            Parameters:  tempMonth - month to change to
+         ************************************************************************/
 	public void setMonth( int tempMonth)
 	{
 		this.month = tempMonth;
 	}
 	
+        /************************************************************************
+            Function:  getYear
+            Author: All
+            Description: Gets year dataset is in
+            Parameters: none
+         ************************************************************************/
 	public int getYear()
 	{
 		return this.year;
 	}
 	
+        /************************************************************************
+            Function:  setYear
+            Author: All
+            Description: Sets year of dataset
+            Parameters: none
+         ************************************************************************/
 	public void setYear( int tempYear )
 	{
 		this.year = tempYear;
 	}
 	
+        /************************************************************************
+            Function:  setDailySamples
+            Author: All
+            Description: Sets data for day of sample
+            Parameters: dayKey - day of the sample
+            * dayOfSamples - data for a day of samples
+         ************************************************************************/
 	public void setDailySamples( int dayKey, Day dayOfSamples )
 	{		
 		this.dailySamples.put( dayKey, dayOfSamples );
 	}
 	
+        /************************************************************************
+            Function:  getAllDaySamples
+            Author: All
+            Description: Gets all samples from all days in dataset
+            Parameters: none
+         ************************************************************************/
 	public Hashtable<Integer, Day> getAllDaySamples()
 	{
 		return this.dailySamples;
 	}
 	
+        /************************************************************************
+            Function:  getDayofSamples
+            Author: All
+            Description: Gets data from a single day of samples
+            Parameters: none
+         ************************************************************************/
 	public Day getDayofSamples( int dayKey)
 	{
 		return this.dailySamples.get(dayKey);
 	}
 	
+        /************************************************************************
+            Function:  reset
+            Author: All
+            Description: sets all the data to null
+            Parameters: none
+         ************************************************************************/
 	public void reset()
 	{
 		this.dailySamples = new Hashtable<Integer, Day>();
